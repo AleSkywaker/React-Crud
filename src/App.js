e747a3aeeaa793ae1ba016282fab9e36826ecd73
Alex Colombo <uricaine@hotmail.com>
2020-05-08 11:38:07 +0200

    init proyect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Parrafo from '../src/components/parrafo';
+import Variables from './components/Variables';
+import { Eventos } from './components/Eventos';
+import Contador from './components/Contador';
+import Lista from './components/Lista';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5'>
+      <h1>Ejercicios React</h1>
+      {/* <Parrafo nombre='Colombo' /> */}
+      <Variables />
+      {/* <Eventos />
+      <Contador />
+      <Lista /> */}
+      <Formulario />
     </div>
   );
 }
