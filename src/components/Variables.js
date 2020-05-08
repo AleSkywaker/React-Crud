f6908929e1be43298d8c739ef3e7cf386490b58e
Alex Colombo <uricaine@hotmail.com>
2020-05-08 11:38:51 +0200

    init proyect

---
+++ src/components/Variables.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Variables = () => {
+  const imagen =
+    'https://sites.google.com/site/surfeerr/_/rsrc/1464264068566/home/maxresdefault.jpg';
+  return (
+    <>
+      <img src={imagen} className='imgSurf' alt='imagen' />
+    </>
+  );
+};
+
+export default Variables;
